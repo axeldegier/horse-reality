e292312ea5b0cdf6aabd568285ce1f39924ca3c1
luuk <hi@eyymail.com>
2021-01-21 14:39:21 +0100

    Code fix
    Haar positionering is gefixed - lichaam wil nog niet,

--- product-page/script.js
+++ product-page/script.js
@@ -9,15 +9,3 @@ $(document).ready(function () {
   });
 
 });
-
-$(document).ready(function () {
-
-  $('.color-choose input').on('click', function () {
-    var headphonesColor = $(this).attr('data-image');
-
-    $('.active').removeClass('active');
-    $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
-    $(this).addClass('active');
-  });
-
-});
